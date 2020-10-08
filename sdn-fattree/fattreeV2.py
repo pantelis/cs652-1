da5f77d82b1da32dca7d8d74ac7b9c3274628af7
tjwags <47124520+tjwags@users.noreply.github.com>
2020-10-08 17:54:14 -0400

    Update fattreeV2.py
    .format applied to host IP on line 41

--- sdn-fattree/fattreeV2.py
+++ sdn-fattree/fattreeV2.py
@@ -38,7 +38,7 @@ class FatTree(Topo):
 					
 			for e in range(len(podedges)): #e = edge switch that hosts will connect to, IP = 10.p.e.x
 				for x in range(2,k/2+1):
-					hip = "10."+str(p)+"."+str(e)+"."+str(x)
+					hip = "10.{}.{}.{}".format(p,e,x)
 					host = self.addHost('h' + str(hc))
 					host.setIP(self,hip,prefixLen=24)
 					podhosts.append(host)
