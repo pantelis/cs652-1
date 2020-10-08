71dc31e4f031317f3ac82aa91349f199c394fa45
tjwags <47124520+tjwags@users.noreply.github.com>
2020-10-08 19:32:33 -0400

    Update fattreeV2.py
    missing hosts

--- sdn-fattree/fattreeV2.py
+++ sdn-fattree/fattreeV2.py
@@ -39,9 +39,9 @@ class FatTree(Topo):
 			for e in range(len(podedges)): #e = edge switch that hosts will connect to, IP = 10.p.e.x
 				for x in range(2,k/2+1):
 					hip = "10.{}.{}.{}".format(p,e,x)
-					host = self.addHost('h' + str(hc), ip=hip, prefixLen=24)
+					podhosts.append(self.addHost('h' + str(hc), ip=hip, prefixLen=24))
 					#host.setIP(self,hip,prefixLen=24)
-					podhosts.append(host)
+					#podhosts.append(host)
 					hc += 1
 			
 			pods[p]["aggs"] = podaggs
